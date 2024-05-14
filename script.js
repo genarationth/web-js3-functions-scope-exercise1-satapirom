16c1bbe4526903fb0725e1e27a8842f2d598c429
Satapirom Khawatthu. <Satapirom.12@gmail.com>
2024-05-14 14:11:42 +0700

    function scrope exercise 1

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(a,b) {
+  return (a + b)   
+}
+console.log(add(15,10));
+
+function subtract (a,b) {
+  return (a - b)   
+}
+console.log(subtract(15,10));
+
+function multiply (a,b) {
+  return (a * b)   
+}
+console.log(multiply(15,10));
+
+function divide (a,b) {
+  return (a / b)   
+}
+console.log(divide(15,10));
+
+//Exercise 1.2
+function me(firstName, location, hobby ){
+    console.log("Hi, my name is " + firstName + " " + "I live in " + location + "and enjoy "+ hobby);
+}
+ me("ying", "thailind", "sleepy");
